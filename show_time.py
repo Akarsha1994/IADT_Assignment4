a7f4e92fb5a438a598edd936d030d429d73b292d
Akarsha Sudheer <sudheer.a@northeastern.edu>
2022-07-01 10:48:33 -0700

    edited show_time.py to add date

--- show_time.py
+++ show_time.py
@@ -1,6 +1,9 @@
 from datetime import datetime
+from datetime import date
 
 now = datetime.now()
+today = date.today()
 
 current_time = now.strftime("%H:%M:%S")
 print("Current Time =", current_time)
+print("Today's date:", today)
